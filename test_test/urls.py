cce753e122f442ce47d81496b69e19b88be058b7
miheishe <dael@bk.ru>
2019-01-30 20:51:08 +0300

    web begin

--- test_test/urls.py
+++ test_test/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
-from tttx.views import test
+# from tttx.views import test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', test),
+    # path('', include('tttx.urls')),
 ]
--- tttx/views.py
+++ tttx/views.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 from django.db import transaction
 
-
 # Create your views here.
 from django.http import HttpResponse
 
@@ -11,6 +10,14 @@ from tttx.models import WaContact
 
 @transaction.atomic
 def test(request):
-
     return HttpResponse([item.name for item in list(WaContact.objects.all())])
 
+
+# def albums_list(request):
+#     albums = Post.objects.all()
+#     return render(request, 'blog/index.html', context={'posts': albums})
+#
+#
+# def album_detail(request, slug):
+#     album = Post.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/post_detail.html', context={'post': album})
